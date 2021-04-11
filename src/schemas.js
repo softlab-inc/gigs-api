e7a9e1d0757757cc7f14757149659b7942a1f528
Raymond <rayjxclusive@gmail.com>
2021-04-12 01:08:45 +0300

    create jobseeker mutation Type and Input fields

--- src/schemas.js
+++ src/schemas.js
@@ -3,13 +3,42 @@
 module.exports = gql`
   scalar DateTime
 
+  #Types
+  type JobSeeker{
+    id:Int
+    fullName:String
+    bio:String
+    email:String
+    phone:String
+    profileImage:String
+    documents:String
+    nationalId:String
+    status:Int
+    createdAt:DateTime
+    updatedAt:DateTime
+  }
+
+  #Queries
   type Query{
    test:String
 
  }
 
+ #InputFields
+ input CreateJobSeekerInput{
+   fullName:String!
+    bio:String
+    email:String!
+    phone:String!
+    profileImage:String
+    documents:Upload!
+    nationalId:Upload!
+ }
+
+ #Mutations 
   type Mutation{
   test(value:String):String
+  createJobSeeker(input:CreateJobSeekerInput):JobSeeker
 
 }
   `;
