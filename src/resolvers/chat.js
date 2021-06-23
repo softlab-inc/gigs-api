70ce925304c324b2d001261e3c9e89ba3e20400f
Raymond <rayjxclusive@gmail.com>
2021-06-23 18:04:32 +0300

    make it better

--- src/resolvers/chat.js
+++ src/resolvers/chat.js
@@ -2,11 +2,10 @@
 module.exports = {
   user: async ({  from,fullName:name}, args, {user}) => {
   
-  
     return {
       _id: (name.length+from),
       name: name,
       avatar: null,
     };
   }
-} 
+}  
