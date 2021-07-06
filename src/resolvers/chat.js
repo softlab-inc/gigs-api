787f8115e77b76ad98e08e5112187dbfe4a218d4
Raymond <rayjxclusive@gmail.com>
2021-07-06 13:38:04 +0300

    update clean up on messaging side

--- src/resolvers/chat.js
+++ src/resolvers/chat.js
@@ -1,22 +1,11 @@
 
 module.exports = {
   user: async ({  from,to,fullName:name,avatar}, args, {user:{id}}) => {
-    console.log({ from,to,fullName:name,avatar})
-    console.log({ from,to, name,id });
-     
-    if (to == from) {
+
       return {
       _id: (name.length+from),
       name: name,
       avatar,
     };
-    } else {
-       return {
-      _id: from,
-      name: name,
-      avatar,
-    };
-    }
-
   }
 }  
