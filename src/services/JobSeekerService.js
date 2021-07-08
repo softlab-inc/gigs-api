49f8647b108344e324aac1b16e0942ca4c5c1609
Raymond <rayjxclusive@gmail.com>
2021-07-08 10:51:36 +0300

    update getNotifications fix returned data

--- src/services/JobSeekerService.js
+++ src/services/JobSeekerService.js
@@ -33,7 +33,7 @@ class JobSeekerSerivce{
   async getNotifications({ employeeId}) {
     const {gig,notified,} = this.models;
     let data = await notified.findAll({where:{employeeId}, include: [gig],order: [['createdAt', 'DESC']] });
-    return data.map(data => ({...data.get('gig').dataValues,status:data.status}));
+    return data.map(data => ({...data.get('gig').dataValues,...data}));
   }
 
   async getReadNotifications({employeeId}) {
