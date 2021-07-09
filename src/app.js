b0712dc8b55481a6d698b28ed1152d11652eb41f
Raymond Kalumba Joseph <34602861+raj360@users.noreply.github.com>
2021-07-09 12:35:04 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -77,13 +77,6 @@ server.applyMiddleware({
  */
 
 
-/**
- 
-  json: {limit: '50mb', extended: true},
-  urlencoded: {limit: '50mb', extended: true}
- 
- 
- */
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', parameterLimit: 100000, extended: true }));
 app.use(logger('dev'));
