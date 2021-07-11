8c99e63749fee1ce71e6655932409a2d912076df
Raymond <rayjxclusive@gmail.com>
2021-07-11 12:09:40 +0300

    update to email infor

--- src/services/MailerService.js
+++ src/services/MailerService.js
@@ -20,17 +20,17 @@ class MailerService{
       from: process.env.MAILER_USER, // sender address
       to: email, // list of receivers
       subject: "Forgot password", // Subject line
-      text: "Use link in the description to updated your password", // plain text body
+      text: "use this infor to update your password", // plain text body
       html: `
       
-  <html>
-    <body>
-    <h5> <i>Click</i> the  link in the description to updated your password </h5>
-    
-    <p> This link will redirect you to a web page where you are required to update your password from</p>
-    <i><a href="url">http://gigsapp.com/support/?id=${id}</a></i>
-  </body>
-</html>
+      <html>
+        <body>
+        <h5> <i>Click</i> the  link in the description to updated your password </h5>
+        
+        <p> This link will redirect you to a web page where you are required to update your password from</p>
+        <i><a href="url">http://gigsapp.com/support/?id=${id}</a></i>
+      </body>
+    </html>
       
       `, // html body
     });
