709c3658c683c68d463067c953c7f95090d61b6a
Raymond <rayjxclusive@gmail.com>
2021-07-28 16:41:55 +0300

    updated JobSeekerService getMessageSenders

--- src/services/EmployerService.js
+++ src/services/EmployerService.js
@@ -308,7 +308,7 @@ async uploadProfileImage({ user, profileImage }) {
   }
   
   async getMessageSenders({user}){
-      this.isAuthenticatic(user);
+    this.isAuthenticatic(user);
     let data =  await this.models.chat.findAll({ where: { employeeId: user.id  },include:['employer'],group:['employerId']});
     return data.map(data => ({ ...data.get('employer').dataValues, ...data.dataValues }));
   }
