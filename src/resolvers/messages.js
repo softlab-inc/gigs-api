8d0eb948a9bc0ddf7335326910476c8f9cd37ada
Raymond <rayjxclusive@gmail.com>
2021-07-30 16:52:58 +0300

    update set-up query to pick up chats

--- src/resolvers/messages.js
+++ src/resolvers/messages.js
@@ -1,6 +1,6 @@
 module.exports = {
 chats:async ({employeeId,employerId},args, { models,user }) => {
-  let chats =   await models.chat.findAll({ where: { employeeId, employerId }, order: [['createdAt', 'DESC']],limit: 40 })
+  let chats =   await models.chat.findAll({ where: { employeeId, employerId }, order: [['createdAt', 'DESC']],limit: 20 })
   return chats.map(data => ({ _id: data.id, text: data.content, ...data.dataValues }));
 },
 
