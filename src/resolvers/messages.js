c19bd9cefddacae4a9f9622cf09b416ba456dd20
Raymond <rayjxclusive@gmail.com>
2021-08-01 14:35:12 +0300

    update limit  messages to 1

--- src/resolvers/messages.js
+++ src/resolvers/messages.js
@@ -1,6 +1,6 @@
 module.exports = {
 chats:async ({employeeId,employerId},args, { models,user }) => {
-  let chats =   await models.chat.findAll({ where: { employeeId, employerId }, order: [['createdAt', 'DESC']],limit: 20 })
+  let chats =   await models.chat.findAll({ where: { employeeId, employerId }, order: [['createdAt', 'DESC']],limit: 1 })
   return chats.map(data => ({ _id: data.id, text: data.content, ...data.dataValues }));
 },
 
