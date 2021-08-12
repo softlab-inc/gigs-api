686bcb152bfe6d67f35dc1d3f4cf6b587c27c098
Raymond <rayjxclusive@gmail.com>
2021-08-12 14:14:57 +0300

    udpate fixed send mail class

--- src/services/MailerService.js
+++ src/services/MailerService.js
@@ -8,21 +8,27 @@ sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
 class MailerService{
 
 
-  const msg = {
-  to: email, // Change to your recipient
-  from: process.env.SENDER_EMAIL, // Change to your verified sender
-  subject:"Forgot password",
-  text: "Use this  to update your password",
-  html: `
-          <html>
-        <body>
-        <h5> <i>Click</i> the  link in the description to updated your password </h5>
-        
-        <a href="https://gigs-app-forgot-password.herokuapp.com/?id=${id}&isEmployer=${isEmployer}">This link will redirect you to a web page where you are required to update your password from</a>
-      </body>
-    </html>
-  `,
-}
+    
+     async sendEmail({email,id,isEmployer}){
+     
+       const msg = {
+          to: email, // Change to your recipient
+          from: process.env.SENDER_EMAIL, // Change to your verified sender
+          subject:"Forgot password",
+          text: "Use this  to update your password",
+          html: `
+                  <html>
+                <body>
+                <h5> <i>Click</i> the  link in the description to updated your password </h5>
+                
+                <a href="https://gigs-app-forgot-password.herokuapp.com/?id=${id}&isEmployer=${isEmployer}">This link will redirect you to a web page where you are required to update your password from</a>
+              </body>
+            </html>
+          `,
+  }
+     
+     
+     }
 
 
 
