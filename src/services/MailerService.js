0ccb7336c3e0a3091dd2a6e0f779066660627df6
Raymond <rayjxclusive@gmail.com>
2021-08-12 14:12:57 +0300

    update added new mailer serivice and finished set up

--- src/services/MailerService.js
+++ src/services/MailerService.js
@@ -1,20 +1,55 @@
 const nodemailer = require('nodemailer');
 
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
+
 
 class MailerService{
+
+
+  const msg = {
+  to: email, // Change to your recipient
+  from: process.env.SENDER_EMAIL, // Change to your verified sender
+  subject:"Forgot password",
+  text: "Use this  to update your password",
+  html: `
+          <html>
+        <body>
+        <h5> <i>Click</i> the  link in the description to updated your password </h5>
+        
+        <a href="https://gigs-app-forgot-password.herokuapp.com/?id=${id}&isEmployer=${isEmployer}">This link will redirect you to a web page where you are required to update your password from</a>
+      </body>
+    </html>
+  `,
+}
   
    async sendMail({email,id,isEmployer}) {
+   
+   
+   
+   
+       
      
      try {
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
-      service: 'SendinBlue',
-      auth: {
+    host: 'SendinBlue',
+    port: 465,
+    secure: true,
+     auth: {
         user: process.env.MAILER_USER,
         pass: process.env.MAILER_PASSWORD
-      }
+      },
+    tls: {
+      ciphers : 'SSLv3',
+    },
     });
        
+      //  console.log({  auth: {
+      //   user: process.env.MAILER_USER,
+      //   pass: process.env.MAILER_PASSWORD
+      // }})
   
     // send mail with defined transport object
     let info = await transporter.sendMail({ 
@@ -34,11 +69,16 @@ class MailerService{
     });
         return info.messageId;
      } catch (error) {
-       console.error(error);
+       console.error({error});
        throw new Error(`${error}`); 
      }
      
     }
+    
+    
+    
+    
+    
 
 }
 module.exports = MailerService;
