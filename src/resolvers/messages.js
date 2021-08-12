16b44414e428115e6a8db48dee5ac85f087ebd9e
Raymond <rayjxclusive@gmail.com>
2021-08-12 13:10:02 +0300

    make it better

--- src/resolvers/messages.js
+++ src/resolvers/messages.js
@@ -3,5 +3,4 @@ chats:async ({employeeId,employerId},args, { models,user }) => {
   let chats =   await models.chat.findAll({ where: { employeeId, employerId }, order: [['createdAt', 'DESC']],limit: 1 })
   return chats.map(data => ({ _id: data.id, text: data.content, ...data.dataValues }));
 }, 
-
 }; 
