0f5c4030abbeb46b25398a6a7f14f7ca9cd8ad4f
Raymond <rayjxclusive@gmail.com>
2021-08-18 08:38:42 +0300

    have linted all my code success fully

--- migrations/2-update_remove_uniqu_key_phone.js
+++ migrations/2-update_remove_uniqu_key_phone.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-var Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 /**
  * Actions summary:
@@ -10,113 +10,113 @@ var Sequelize = require('sequelize');
  *
  **/
 
-var info = {
-    "revision": 2,
-    "name": "update_remove_uniqu_key_phone",
-    "created": "2021-08-18T05:13:39.529Z",
-    "comment": ""
+const info = {
+  revision: 2,
+  name: "update_remove_uniqu_key_phone",
+  created: "2021-08-18T05:13:39.529Z",
+  comment: "",
 };
 
-var migrationCommands = function(transaction) {
-    return [{
-            fn: "removeIndex",
-            params: [
-                "employer",
-                "phone",
-                {
-                    transaction: transaction
-                }
-            ]
+const migrationCommands = function (transaction) {
+  return [
+    {
+      fn: "removeIndex",
+      params: [
+        "employer",
+        "phone",
+        {
+          transaction: transaction,
+        },
+      ],
+    },
+    {
+      fn: "changeColumn",
+      params: [
+        "employer",
+        "phone",
+        {
+          type: Sequelize.STRING(15),
+          field: "phone",
+          allowNull: true,
         },
         {
-            fn: "changeColumn",
-            params: [
-                "employer",
-                "phone",
-                {
-                    "type": Sequelize.STRING(15),
-                    "field": "phone",
-                    "allowNull": true
-                },
-                {
-                    transaction: transaction
-                }
-            ]
-        }
-    ];
+          transaction: transaction,
+        },
+      ],
+    },
+  ];
 };
-var rollbackCommands = function(transaction) {
-    return [{
-            fn: "changeColumn",
-            params: [
-                "employer",
-                "phone",
-                {
-                    "type": Sequelize.STRING(15),
-                    "field": "phone",
-                    "unique": "phone",
-                    "allowNull": true
-                },
-                {
-                    transaction: transaction
-                }
-            ]
+const rollbackCommands = function (transaction) {
+  return [
+    {
+      fn: "changeColumn",
+      params: [
+        "employer",
+        "phone",
+        {
+          type: Sequelize.STRING(15),
+          field: "phone",
+          unique: "phone",
+          allowNull: true,
         },
         {
-            fn: "addIndex",
-            params: [
-                "employer",
-                [{
-                    "name": "phone"
-                }],
-                {
-                    "indexName": "phone",
-                    "name": "phone",
-                    "indicesType": "UNIQUE",
-                    "type": "UNIQUE",
-                    "transaction": transaction
-                }
-            ]
-        }
-    ];
+          transaction: transaction,
+        },
+      ],
+    },
+    {
+      fn: "addIndex",
+      params: [
+        "employer",
+        [
+          {
+            name: "phone",
+          },
+        ],
+        {
+          indexName: "phone",
+          name: "phone",
+          indicesType: "UNIQUE",
+          type: "UNIQUE",
+          transaction: transaction,
+        },
+      ],
+    },
+  ];
 };
 
 module.exports = {
-    pos: 0,
-    useTransaction: true,
-    execute: function(queryInterface, Sequelize, _commands)
-    {
-        var index = this.pos;
-        function run(transaction) {
-            const commands = _commands(transaction);
-            return new Promise(function(resolve, reject) {
-                function next() {
-                    if (index < commands.length)
-                    {
-                        let command = commands[index];
-                        console.log("[#"+index+"] execute: " + command.fn);
-                        index++;
-                        queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
-                    }
-                    else
-                        resolve();
-                }
-                next();
-            });
+  pos: 0,
+  useTransaction: true,
+  execute: function (queryInterface, Sequelize, _commands) {
+    let index = this.pos;
+    function run(transaction) {
+      const commands = _commands(transaction);
+      return new Promise(function (resolve, reject) {
+        function next() {
+          if (index < commands.length) {
+            const command = commands[index];
+            console.log("[#" + index + "] execute: " + command.fn);
+            index++;
+            queryInterface[command.fn]
+              .apply(queryInterface, command.params)
+              .then(next, reject);
+          } else resolve();
         }
-        if (this.useTransaction) {
-            return queryInterface.sequelize.transaction(run);
-        } else {
-            return run(null);
-        }
-    },
-    up: function(queryInterface, Sequelize)
-    {
-        return this.execute(queryInterface, Sequelize, migrationCommands);
-    },
-    down: function(queryInterface, Sequelize)
-    {
-        return this.execute(queryInterface, Sequelize, rollbackCommands);
-    },
-    info: info
+        next();
+      });
+    }
+    if (this.useTransaction) {
+      return queryInterface.sequelize.transaction(run);
+    } else {
+      return run(null);
+    }
+  },
+  up: function (queryInterface, Sequelize) {
+    return this.execute(queryInterface, Sequelize, migrationCommands);
+  },
+  down: function (queryInterface, Sequelize) {
+    return this.execute(queryInterface, Sequelize, rollbackCommands);
+  },
+  info: info,
 };
--- seeders/20210818051801-default-professions.js
+++ seeders/20210818051801-default-professions.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -10,42 +10,45 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-    
-      await queryInterface.bulkInsert('profession',[
-{name:"Accounting"},
-{name:"Administration"},
-{name:"Acting "},
-{name:"Aviation"},
-{name:"Aeronautics"},
-{name:"Back-End Developer"},
-{name:"Branding"},
-{name:"Carpenter"},
-{name:"Chef"},
-{name:"Copyright "},
-{name:"Customer Service "},
-{name:"Decorator "},
-{name:"Executive Assistant"},
-{name:"Event organizer"},
-{name:"Financial Analyst"},
-{name:"Front-End Developer"},
-{name:"Full-Stack Engineer"},
-{name:"Graphic Designer"},
-{name:"Marketing "},
-{name:"Musician "},
-{name:"Office Management"},
-{name:"Product Manager"},
-{name:"Project Manager"},
-{name:"Proposal Writing "},
-{name:"Photography"},
-{name:"Public speaking"},
-{name:"Salesperson"},
-{name:"Teaching "},
-{name:"Training"},
-{name:"Web Developer"},
-{name:"Other "},
-], {});
-    
+     */
+
+    await queryInterface.bulkInsert(
+      "profession",
+      [
+        { name: "Accounting" },
+        { name: "Administration" },
+        { name: "Acting " },
+        { name: "Aviation" },
+        { name: "Aeronautics" },
+        { name: "Back-End Developer" },
+        { name: "Branding" },
+        { name: "Carpenter" },
+        { name: "Chef" },
+        { name: "Copyright " },
+        { name: "Customer Service " },
+        { name: "Decorator " },
+        { name: "Executive Assistant" },
+        { name: "Event organizer" },
+        { name: "Financial Analyst" },
+        { name: "Front-End Developer" },
+        { name: "Full-Stack Engineer" },
+        { name: "Graphic Designer" },
+        { name: "Marketing " },
+        { name: "Musician " },
+        { name: "Office Management" },
+        { name: "Product Manager" },
+        { name: "Project Manager" },
+        { name: "Proposal Writing " },
+        { name: "Photography" },
+        { name: "Public speaking" },
+        { name: "Salesperson" },
+        { name: "Teaching " },
+        { name: "Training" },
+        { name: "Web Developer" },
+        { name: "Other " },
+      ],
+      {}
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -55,9 +58,5 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
-
-
-
-
