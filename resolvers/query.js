2a00f895c410c0fea5c5fcdbe9a724b8a607f7ed
Raymond <rayjxclusive@gmail.com>
2021-09-21 17:57:47 +0300

    updated all queries with global service instance

--- resolvers/query.js
+++ resolvers/query.js
@@ -1,19 +1,16 @@
-const { JobSeekerSerivce, EmployerService } = require("../services");
-
 module.exports = {
-  jobSeeker: async (_, __, { models, user }) => {
-    const jobSeekerService = new JobSeekerSerivce(models);
-    return await jobSeekerService.jobSeeker({ user });
+  jobSeeker: async (_, __, { services:{JobSeekerService}, user }) => {
+    return await JobSeekerService.jobSeeker({ user });
   },
+  
   professions: async (_, __, { models }) => await models.profession.findAll(),
-  employer: async (_, __, { models, user }) => {
-    const employerService = new EmployerService(models);
-    return await employerService.employer({ user });
+  employer: async (_, __, { services:{EmployerService}, user }) => {
+    return await EmployerService.employer({ user });
   },
-  employerChats: async (_, { employeeId }, { models, user }) => {
-    const employerService = new EmployerService(models);
+  
+  employerChats: async (_, { employeeId }, { services:{EmployerService}, user }) => {
     console.log({ employeeId, user });
-    const chats = await employerService.getChats({ user, employeeId });
+    const chats = await EmployerService.getChats({ user, employeeId });
     console.log({ chats });
     return chats.map((data) => ({
       _id: data.id,
@@ -21,57 +18,60 @@ module.exports = {
       ...data.dataValues,
     }));
   },
-  jobSeekerChats: async (_, { employerId }, { models, user }) => {
-    const jobSeekerService = new JobSeekerSerivce(models);
-    const chats = await jobSeekerService.getChats({ user, employerId });
+  
+  jobSeekerChats: async (_, { employerId }, { services:{JobSeekerService}, user }) => {
+    const chats = await JobSeekerService.getChats({ user, employerId });
     return chats.map((data) => ({
       _id: data.id,
       text: data.content,
       ...data.dataValues,
     }));
   },
-  gig: async (_, __, { models, user }) => {
-    const employerService = new EmployerService(models);
-    return await employerService.getCreatedGig({ user });
+  
+  gig: async (_, __, { services:{EmployerService}, user }) => {
+    return await EmployerService.getCreatedGig({ user });
   },
-  createdGigs: async (_, __, { models, user }) => {
-    const employerService = new EmployerService(models);
-    return await employerService.getCreatedGigs({ user });
+  createdGigs: async (_, __, { services:{EmployerService}, user }) => {
+    return await EmployerService.getCreatedGigs({ user });
   },
-  getGig: async (_, { id }, { models, user }) =>
-    models.gig.findOne({ where: id }), // hasAccepted -> wasHired for job
+  // hasAccepted -> wasHired for job
+  getGig: async (_, { id }, { models }) => models.gig.findOne({ where: id }), 
+  
   employerNotifications: async (_, __, { models, user }) =>
     await models.accepted.findAll({
       where: { employerId: user.id, hasAccepted: 0 },
       order: [["createdAt", "DESC"]],
       limit: 20,
     }),
-  jobSeekerNotifications: async (_, __, { models, user }) => {
+    
+  jobSeekerNotifications: async (_, __, { services:{}, user }) => {
     const jobSeekerService = new JobSeekerSerivce(models);
     return await jobSeekerService.getAllNotifications({ user });
   },
-  recentHires: async (_, __, { models, user }) => {
+  
+  recentHires: async (_, __, { services:{}, user }) => {
     const employerService = new EmployerService(models);
     return await employerService.getRecentHires({ user });
   },
-  employerMessages: async (_, __, { models, user }) => {
+  
+  employerMessages: async (_, __, { services:{}, user }) => {
     const employerService = new EmployerService(models);
     return await employerService.getMessageSenders({ user });
   },
-  jobSeekerMessages: async (_, __, { models, user }) => {
-    const jobSeekerService = new JobSeekerSerivce(models);
-    return await jobSeekerService.getMessageSenders({ user });
+  
+  jobSeekerMessages: async (_, __, { services:{JobSeekerService}, user }) => {
+    return await JobSeekerService.getMessageSenders({ user });
   },
-  pendingGigs: async (_, __, { models, user }) => {
-    const jobSeekerService = new JobSeekerSerivce(models);
-    const result = await jobSeekerService.getPendingGigs({
+  
+  pendingGigs: async (_, __, { services:{JobSeekerService}, user }) => {
+    const result = await JobSeekerService.getPendingGigs({
       employeeId: user.id,
     });
     console.log({ result });
     return result;
   },
-  getGetJobSeeker: async (_, { id }, { models }) => {
-    const jobSeekerService = new JobSeekerSerivce(models);
-    return await jobSeekerService.jobSeeker({ user: { id } });
+  
+  getGetJobSeeker: async (_, { id }, { services: {JobSeekerService}}) => {
+    return await jobSeekerService.JobSeeker({ user: { id } });
   },
 };
