7d4937f4b749d5ab51a2b3014f85b3fd9a6913f5
Raymond Kalumba Joseph <raymondkalumba360@gmail.com>
2021-11-04 12:06:24 +0300

    update added isStarted

--- resolvers/gig.js
+++ resolvers/gig.js
@@ -8,9 +8,11 @@ module.exports = {
       where: { gigId: id },
       include: [models.employee],
     });
+
     return data.map((data) => ({
       ...data.get("employee").dataValues,
       status: data.status,
+      isStarted:data.isStarted
     })); // update employee onLine status with gig progress status
   },
 };
