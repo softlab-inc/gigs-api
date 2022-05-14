bcb73c377559ff63bdf4e7a31bcf8e6acab17694
raj360 <raymondkalumba360@gmail.com>
2022-05-14 20:15:53 +0300

    removed unused code

--- services/AWS3Service.js
+++ services/AWS3Service.js
@@ -1,32 +1,4 @@
-// const { ForbiddenError } = require("apollo-server-express");
-
-// const MAX_UPLOAD_FILE_SIZE = 1048576;
-
-const AWS = require("aws-sdk");
-// store each image in it's own unique folder to avoid name duplicates
-
-const { v4: uuidv4 } = require("uuid");
-
-// AWS config info
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
-  region: process.env.AWS_S3_REGION,
-  bucket: process.env.AWS_S3_BUCKET,
-});
-
-const s3 = new AWS.S3({ region: process.env.AWS_S3_REGION });
-
-// my default params for s3 upload
-// I have a max upload size of 1 MB
-const s3DefaultParams = {
-  ACL: "public-read",
-  Bucket: process.env.AWS_S3_BUCKET,
-  Conditions: [
-    ["content-length-range", 0, 1024000], // 1 Mb
-    { acl: "public-read" },
-  ],
-};
+const { v4: uuidv4 } = require('uuid');
 
 // the actual upload happens here
 const handleFileUpload = async (file) => {
@@ -42,24 +14,7 @@ const handleFileUpload = async (file) => {
 
   const key = uuidv4();
 
-  return new Promise((resolve, reject) => {
-    s3.upload(
-      {
-        ...s3DefaultParams,
-        Body: createReadStream(),
-        Key: `${key}/${filename}`,
-      },
-      (err, data) => {
-        if (err) {
-          console.log("error uploading...", err);
-          reject(err);
-        } else {
-          console.log("successfully uploaded file...", data);
-          resolve(data);
-        }
-      }
-    );
-  });
+  return key;
 };
 
 // const findStreamSize = (_buffer) => {
